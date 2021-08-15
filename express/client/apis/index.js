9eb24098d3e8fa9f614b013fd522a479f1917b05
jordan-tuinman <jojotuijo@gmail.com>
2021-08-15 17:32:56 +1200

    create api get requests

--- express/client/apis/index.js
+++ express/client/apis/index.js
@@ -2,10 +2,14 @@ import request from 'superagent'
 
 // Get list of posts - title and slug - JSON Array
 export const getPostsList = () => {
-  return request.get()
+  return request.get('/posts').then((res) => {
+    return res.body
+  })
 }
 
 // Get post content - JSON
-export const getPost = () => {
-  return request.get()
+export const getPost = (slug) => {
+  return request.get(`/post/${slug}`).then((res) => {
+    return res.body
+  })
 }
